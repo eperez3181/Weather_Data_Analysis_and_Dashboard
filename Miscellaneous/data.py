9468dfa97c0b62f06336446c874a64634bc8f782
eperez3181 <eperez3181@yahoo.com>
2022-06-25 17:36:40 -0700

    adding files

---
+++ Miscellaneous/data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Read the csv file in
+df = pd.read_csv(r"C:\Users\jlper\Web\Web-Design-Challenge\Resources\cities.csv", delimiter=",")
+
+df.reset_index(inplace=True)
+df.drop(['index', 'Index'], axis=1, inplace=True)
+
+#print(df.columns)
+
+# Save to file
+df.to_html('data_table.html', index=False)
+
+# Assign to string
+#table = df.to_html()
+#print(table)
